09d77f7438c6e60d9b4e37c1dda190a1c95fba6b
Catmhhw <catherine.mhh.wu@gmail.com>
2023-02-18 18:18:35 -0500

    add tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validation" do
+
+    it "should" do
+      user = User.create(
+        email: "buttercake@email.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+      expect(user).to be_valid
+    end
+
+end
