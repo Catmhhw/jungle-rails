e3ade836b9fed8df4dfcce4ac5b93c41c173a7a7
Catmhhw <catherine.mhh.wu@gmail.com>
2023-02-18 18:32:18 -0500

    remove test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,18 +15,6 @@ RSpec.describe Product, type: :model do
       expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
-      it 'fails to save without a name' do
-      category = Category.create(name: 'Flowers')
-      product = Product.new(
-        name: nil,
-        price: 25,
-        quantity: 20,
-        category: category
-      )
-      expect(product.save).to be false
-      expect(product.errors.full_messages).to include("Name can't be blank")
-    end
-
     it 'should fail to save without a price' do
       category = Category.create(name: 'Flowers')
       product = Product.new(
